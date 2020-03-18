2292ef210eed9240b76a4cc415fbef31adb4252f
TPride <749693791>
2020-03-19 03:21:35 +0800

    update

--- src/gopur/Information.java
+++ src/gopur/Information.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public interface Information {
     String NOWPATH = System.getProperty("user.dir").concat(File.separator);
-    String VERSION = "0.0.4";
+    String VERSION = "0.0.5";
     String[] AUTHORS = { "TPride" };
 }
